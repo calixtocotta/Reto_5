7af04bfdedfa2d1fd25fe849310994dd41156c07
calixto cotta <ccottav14@gmail.com>
2021-12-19 10:30:32 -0500

    Reto 5 frontend

---
+++ src/components/paginas/Productos.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import FormularioProductos from '../ui/FormularioProductos';
+
+const Productos = () => {
+    const [ productos, guardarProductos] = useState([]);
+
+    fetch("http://144.22.228.79:8080/api/clothe/all")
+    .then((res) => res.json())
+    .then((data) => {
+        //console.log(data);
+        guardarProductos(data);
+    });
+       return( 
+        <>
+         <h1 className="text-3xl font-light mb-4">Productos</h1>
+            <Link to="/nuevo-producto" className="  bg-blue-800 hover:bg-blue-700, inline-block mb-5 p-2 text-white uppercase font-bold">
+                Agregar Producto
+            </Link>
+            {productos.map( producto =>(
+                <FormularioProductos 
+                key={producto.reference}
+                producto={producto}
+                />
+                
+
+            ))}
+            
+        </>
+     );
+}
+ 
+export default Productos;
